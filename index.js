c57c947b16cfeadc42dc03d0586b62e286769a11
Bruce Bookman <bruce.bookman@gmail.com>
2018-11-19 23:57:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,6 +26,11 @@ function viewCart() {
   if (cart.length == 0){
     return "Your shopping cart is empty."
   }
+  
+  if (cart.length == 1){
+    returnString += item.itemName + " at $" + item.itemPrice
+    return returnString
+  }
   for (var i = 0; i < cart.length; i++){
     var item = cart[i]
     
