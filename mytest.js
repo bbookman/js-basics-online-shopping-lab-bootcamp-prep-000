ad62bd97053f7df5d3765555eaac053408fc10e4
Bruce Bookman <bruce.bookman@gmail.com>
2018-11-20 00:49:54 +0000

    automatically backed up by learn

--- mytest.js
+++ mytest.js
@@ -1,27 +1,57 @@
 cart = [{itemName: "bananas", itemPrice: 10},{itemName: "chocolate", itemPrice: 20}]
+var cart = [];
+
+function getCart() {
+ return cart;
+}
+
+function setCart(c) {
+  cart = c;
+  return cart;
+}
+
+
 function addToCart(item) {
  // write your code here
     var min=1; 
     var max=101;  
     var price =Math.floor(Math.random() * (max - min)) + min;
     cart.push({itemName: item, itemPrice: price})
-    console.log(viewCart())
     return item + " has been added to your cart.";
 }
+
 
 function viewCart() {
   // write your code here
   var returnString = "In your cart, you have "
+  if (cart.length == 0){
+    return "Your shopping cart is empty."
+  }
+  
+  if (cart.length == 1){
+    var item = cart[0]
+    returnString += item.itemName + " at $" + item.itemPrice + "."
+    return returnString
+  }
   for (var i = 0; i < cart.length; i++){
     var item = cart[i]
     
     if ((i + 1) == cart.length){
-      returnString += item.itemName + " at $" + item.itemPrice
+      returnString += "and " + item.itemName + " at $" + item.itemPrice + "."
     } else {
        returnString += item.itemName + " at $" + item.itemPrice + ", "
     }
   }
   return returnString
+}
+
+function total() {
+  var total = 0
+  for (var i=0; i < cart.length; i++){
+    var item = cart[i]
+    total += item.itemPrice
+  }
+  return total
 }
 
 
@@ -36,14 +66,15 @@ function removeFromCart(item) {
   return "That item is not in your cart.";
 }
 
+
 function placeOrder(cardNumber) {
    if (cardNumber !== undefined){
      cart = []
      
-     return "Your total cost is $" + 5 + ", which will be charged to the card " + cardNumber + "." 
+     return "Your total cost is $" + total() + ", which will be charged to the card " + cardNumber + "." 
    } else {
      return "Sorry, we don't have a credit card on file for you."
    }
 }
 
-console.log(placeOrder());
+console.log(placeOrder(5678))
