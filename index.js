b27c958c6442cf2b7b0448d434455fe6dbc19d5d
Bruce Bookman <bruce.bookman@gmail.com>
2018-11-20 00:47:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,11 +67,10 @@ function removeFromCart(item) {
 
 
 function placeOrder(cardNumber) {
-   console.log(cardNumber)
-   if (cardNumber !== null){
+   if (cardNumber !== undefined){
      cart = []
      
-     return "Your total cost is $" + total() + ", which will be charged to the card " + cardNumber + "." 
+     return "Your total cost is $" + 5 + ", which will be charged to the card " + cardNumber + "." 
    } else {
      return "Sorry, we don't have a credit card on file for you."
    }
