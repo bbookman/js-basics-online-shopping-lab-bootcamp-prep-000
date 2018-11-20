21803fb93674061a962188defbdc8bec9787c837
Bruce Bookman <bruce.bookman@gmail.com>
2018-11-20 00:51:52 +0000

    automatically backed up by learn

--- mytest.js
+++ mytest.js
@@ -49,6 +49,7 @@ function total() {
   var total = 0
   for (var i=0; i < cart.length; i++){
     var item = cart[i]
+    console.log("Item " + item.itemName + " Cost $" + item.itemPrice)
     total += item.itemPrice
   }
   return total
