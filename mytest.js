49aace91d6d462db6b42ab78a6cd66ecdddebd3a
Bruce Bookman <bruce.bookman@gmail.com>
2018-11-20 00:46:43 +0000

    automatically backed up by learn

--- mytest.js
+++ mytest.js
@@ -38,7 +38,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
    console.log(cardNumber)
-   if (cardNumber != null){
+   if (cardNumber !== undefined){
      cart = []
      
      return "Your total cost is $" + 5 + ", which will be charged to the card " + cardNumber + "." 
