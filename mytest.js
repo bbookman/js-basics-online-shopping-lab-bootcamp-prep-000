ed3e794fa4ecd1958ced1dd247e2a85c5b1c3db7
Bruce Bookman <bruce.bookman@gmail.com>
2018-11-20 00:55:48 +0000

    automatically backed up by learn

--- mytest.js
+++ mytest.js
@@ -1,16 +1,16 @@
 var cart = [{itemName: "bananas", itemPrice: 10},{itemName: "chocolate", itemPrice: 20}]
 
 
-function total() {
-  var total = 0
-  for (var i=0; i < cart.length; i++){
-    var item = cart[i]
-    console.log("Item " + item.itemName + " Cost $" + item.itemPrice)
-    total += item.itemPrice
-  }
-  return total
+function placeOrder(cardNumber) {
+  console.log("Total is " + total())
+   if (cardNumber !== undefined){
+     cart = []
+     
+     return "Your total cost is $" + total() + ", which will be charged to the card " + cardNumber + "." 
+   } else {
+     return "Sorry, we don't have a credit card on file for you."
+   }
 }
-
 
 
 console.log(total())
